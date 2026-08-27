bb61001b4f067d13a43097640ed00c3b8000cdd0
Farhan <Farhan0629@gmail.com>
2026-08-27 11:38:26 +0530

    build: set custom APK output filename to JharUdyam.apk

--- App/android/app/build.gradle.kts
+++ App/android/app/build.gradle.kts
@@ -39,6 +39,13 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+
+    applicationVariants.all {
+        outputs.all {
+            val output = this as? com.android.build.gradle.internal.api.ApkVariantOutputImpl
+            output?.outputFileName = "JharUdyam.apk"
+        }
+    }
 }
 
 flutter {
